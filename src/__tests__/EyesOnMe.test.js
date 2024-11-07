dc27922f56cd030f3a89302894ca62d699e36d46
blessed wesonga <blessedwesonga@gmail.com>
2024-11-07 23:49:01 +0300

    add m

--- src/__tests__/EyesOnMe.test.js
+++ src/__tests__/EyesOnMe.test.js
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom";
+ import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import EyesOnMe from "../components/EyesOnMe";
 
